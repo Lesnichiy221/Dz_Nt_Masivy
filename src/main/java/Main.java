5ce6a02bf51f22ad57ae462031460d3bd3204288
Vlad Test <vladkretovsky@yandex.ru>
2024-07-21 18:57:37 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] salesPerMonth = {
+                8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18
+        };
+
+    }
+}
